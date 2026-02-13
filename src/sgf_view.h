b2f8f6834803e21c8b5de311818b318a71719c4a
Jerome Abela <Jerome.Abela@gmail.com>
2026-02-13 15:43:22 +0000

    Trim SGF view leaf navigation and no-op rendering code

--- src/sgf_view.h
+++ src/sgf_view.h
@@ -11,19 +11,11 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE(SgfView, sgf_view, SGF, VIEW, GObject)
 
-typedef enum {
-  SGF_VIEW_NAVIGATE_PARENT,
-  SGF_VIEW_NAVIGATE_CHILD,
-  SGF_VIEW_NAVIGATE_PREVIOUS_SIBLING,
-  SGF_VIEW_NAVIGATE_NEXT_SIBLING
-} SgfViewNavigation;
-
 SgfView *sgf_view_new(void);
 GtkWidget *sgf_view_get_widget(SgfView *self);
 void sgf_view_set_tree(SgfView *self, SgfTree *tree);
 void sgf_view_set_selected(SgfView *self, const SgfNode *node);
 const SgfNode *sgf_view_get_selected(SgfView *self);
-gboolean sgf_view_navigate(SgfView *self, SgfViewNavigation navigation);
 void sgf_view_refresh(SgfView *self);
 void sgf_view_force_layout_sync(SgfView *self);
 
