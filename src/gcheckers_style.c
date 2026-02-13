2a3137fbc1543abba70e08bca09288d7c16105cf
Jerome Abela <Jerome.Abela@gmail.com>
2026-02-13 12:41:34 +0000

    Trim leaf UI features to minimal no-op repro

--- src/gcheckers_style.c
+++
@@ -1,7 +0,0 @@
-#include "gcheckers_style.h"
-
-#include <gtk/gtk.h>
-
-void gcheckers_style_init(void) {
-  /* Styling is intentionally disabled to keep the reproduction focused on SGF scrolling behavior. */
-}
