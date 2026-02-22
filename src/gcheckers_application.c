4d93e8c070a1f3d1bfeb95b4931652d780e7e65b
JeromeA <jerome.abela@gmail.com>
2026-02-22 11:30:29 +0000

    Add load/save/import in the menu.

--- src/gcheckers_application.c
+++ src/gcheckers_application.c
@@ -89,10 +89,17 @@ static void gcheckers_application_startup(GApplication *app) {
 
   GMenu *menubar = g_menu_new();
   GMenu *file_menu = g_menu_new();
+  GMenu *file_primary_menu = g_menu_new();
+  GMenu *file_quit_menu = g_menu_new();
   GMenu *game_menu = g_menu_new();
   GMenu *game_navigation_menu = g_menu_new();
-  g_menu_append(file_menu, "New game...", "app.new-game");
-  g_menu_append(file_menu, "Quit", "app.quit");
+  g_menu_append(file_primary_menu, "New game...", "app.new-game");
+  g_menu_append(file_primary_menu, "Import...", NULL);
+  g_menu_append(file_primary_menu, "Load...", NULL);
+  g_menu_append(file_primary_menu, "Save as...", NULL);
+  g_menu_append_section(file_menu, NULL, G_MENU_MODEL(file_primary_menu));
+  g_menu_append(file_quit_menu, "Quit", "app.quit");
+  g_menu_append_section(file_menu, NULL, G_MENU_MODEL(file_quit_menu));
   g_menu_append(game_menu, "Force move", "app.force-move");
   g_menu_append(game_navigation_menu, "Rewind to start", "win.sgf-rewind");
   g_menu_append(game_navigation_menu, "Back one move", "win.sgf-step-backward");
@@ -104,6 +111,8 @@ static void gcheckers_application_startup(GApplication *app) {
   g_menu_append_submenu(menubar, "Game", G_MENU_MODEL(game_menu));
   gtk_application_set_menubar(GTK_APPLICATION(app), G_MENU_MODEL(menubar));
 
+  g_object_unref(file_quit_menu);
+  g_object_unref(file_primary_menu);
   g_object_unref(game_navigation_menu);
   g_object_unref(game_menu);
   g_object_unref(file_menu);
