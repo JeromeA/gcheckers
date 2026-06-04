63aa67b08c61a5922826e8ba79b657782bf67f51
JeromeA <jerome.abela@gmail.com>
2026-06-04 22:36:42 +0100

    Refactor reproducer window cycle

--- bug.c
+++ bug.c
@@ -1,43 +1,33 @@
 #include <gtk/gtk.h>
 
-static GtkWidget *drawer_host;
-
-static GtkWindow *create_window(void) {
+static void create_and_detroy_window(void) {
+  /* Create window. */
   GtkWindow *window = GTK_WINDOW(gtk_window_new());
 
   GtkWidget *paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
   gtk_window_set_child(window, paned);
 
-  drawer_host = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  GtkWidget *drawer_host = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   g_object_ref_sink(drawer_host);
 
   gtk_paned_set_end_child(GTK_PANED(paned), drawer_host);
 
-  return window;
-}
+  gtk_window_present(window);
 
-static void destroy_window(GtkWindow *window) {
-  gtk_paned_set_end_child(GTK_PANED(gtk_widget_get_parent(drawer_host)), NULL);
-  g_clear_object(&drawer_host);
-  gtk_window_destroy(window);
-}
-
-static void drain_main_context(void) {
+  /* Drain main context. */
   for (guint i = 0; i < 256; ++i) {
     g_main_context_iteration(NULL, FALSE);
   }
+
+  /* Destroy window. */
+  gtk_paned_set_end_child(GTK_PANED(gtk_widget_get_parent(drawer_host)), NULL);
+  g_object_unref(drawer_host);
+  gtk_window_destroy(window);
 }
 
 int main(void) {
   gtk_init();
 
-  GtkWindow *window = create_window();
-  gtk_window_present(window);
-  drain_main_context();
-  destroy_window(window);
-
-  window = create_window();
-  gtk_window_present(window);
-  drain_main_context();
-  destroy_window(window);
+  create_and_detroy_window();
+  create_and_detroy_window();
 }
