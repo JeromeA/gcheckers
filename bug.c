77a698580379a76f9a34836b4f554a6ad6e83644
JeromeA <jerome.abela@gmail.com>
2026-06-04 16:05:31 +0100

    Trim reproducer window subclass

--- bug.c
+++ bug.c
@@ -1,68 +1,43 @@
 #include <gtk/gtk.h>
 
-#define GGAME_TYPE_WINDOW (ggame_window_get_type())
+static GtkWidget *main_paned;
+static GtkWidget *drawer_host;
+static GtkWidget *navigation_panel;
 
-G_DECLARE_FINAL_TYPE(GGameWindow, ggame_window, GGAME, WINDOW, GtkApplicationWindow)
-
-struct _GGameWindow {
-  GtkApplicationWindow parent_instance;
-  GtkWidget *main_paned;
-  GtkWidget *drawer_host;
-  GtkWidget *navigation_panel;
-};
-
-G_DEFINE_TYPE(GGameWindow, ggame_window, GTK_TYPE_APPLICATION_WINDOW)
-
-static void ggame_window_dispose(GObject *object) {
-  GGameWindow *self = GGAME_WINDOW(object);
-
-  if (self->navigation_panel != NULL) {
-    gtk_box_remove(GTK_BOX(self->drawer_host), self->navigation_panel);
-    g_clear_object(&self->navigation_panel);
-  }
-  if (self->drawer_host != NULL) {
-    gtk_paned_set_end_child(GTK_PANED(gtk_widget_get_parent(self->drawer_host)), NULL);
-    g_clear_object(&self->drawer_host);
-  }
-  G_OBJECT_CLASS(ggame_window_parent_class)->dispose(object);
-}
-
-static void ggame_window_class_init(GGameWindowClass *klass) {
-  GObjectClass *object_class = G_OBJECT_CLASS(klass);
-
-  object_class->dispose = ggame_window_dispose;
-}
-
-static void ggame_window_init(GGameWindow *self) {
-  gtk_window_set_default_size(GTK_WINDOW(self), 1260, 700);
+static GtkWindow *create_window(GtkApplication *app) {
+  GtkWindow *window = GTK_WINDOW(gtk_application_window_new(app));
+  gtk_window_set_default_size(window, 1260, 700);
 
   GtkWidget *paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_window_set_child(GTK_WINDOW(self), paned);
-  self->main_paned = paned;
+  gtk_window_set_child(window, paned);
+  main_paned = paned;
 
   GtkWidget *left_panel = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   gtk_paned_set_start_child(GTK_PANED(paned), left_panel);
 
-  GtkWidget *drawer_host = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  drawer_host = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   g_object_ref_sink(drawer_host);
   gtk_paned_set_end_child(GTK_PANED(paned), drawer_host);
-  self->drawer_host = drawer_host;
 
   GtkWidget *middle_panel = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   g_object_ref_sink(middle_panel);
-  self->navigation_panel = middle_panel;
+  navigation_panel = middle_panel;
 
   gtk_paned_set_end_child(GTK_PANED(paned), NULL);
   gtk_box_append(GTK_BOX(drawer_host), middle_panel);
   gtk_paned_set_end_child(GTK_PANED(paned), drawer_host);
   gtk_widget_set_size_request(left_panel, 760, -1);
   gtk_paned_set_position(GTK_PANED(paned), 960);
+
+  return window;
 }
 
-static GGameWindow *ggame_window_new(GtkApplication *app) {
-  g_return_val_if_fail(GTK_IS_APPLICATION(app), NULL);
-
-  return g_object_new(GGAME_TYPE_WINDOW, "application", app, NULL);
+static void destroy_window(GtkWindow *window) {
+  gtk_box_remove(GTK_BOX(drawer_host), navigation_panel);
+  g_clear_object(&navigation_panel);
+  gtk_paned_set_end_child(GTK_PANED(gtk_widget_get_parent(drawer_host)), NULL);
+  g_clear_object(&drawer_host);
+  gtk_window_destroy(window);
 }
 
 static void drain_main_context(void) {
@@ -80,16 +55,16 @@ int main(void) {
                                             G_APPLICATION_DEFAULT_FLAGS | G_APPLICATION_NON_UNIQUE);
   g_application_register(G_APPLICATION(app), NULL, NULL);
 
-  GGameWindow *window = ggame_window_new(app);
-  gtk_window_present(GTK_WINDOW(window));
+  GtkWindow *window = create_window(app);
+  gtk_window_present(window);
   drain_main_context();
-  gtk_window_destroy(GTK_WINDOW(window));
+  destroy_window(window);
 
-  window = ggame_window_new(app);
-  gtk_window_set_default_size(GTK_WINDOW(window), 2000, 700);
-  gtk_window_present(GTK_WINDOW(window));
+  window = create_window(app);
+  gtk_window_set_default_size(window, 2000, 700);
+  gtk_window_present(window);
   drain_main_context();
-  gtk_paned_set_position(GTK_PANED(window->main_paned), 1400);
-  gtk_window_destroy(GTK_WINDOW(window));
+  gtk_paned_set_position(GTK_PANED(main_paned), 1400);
+  destroy_window(window);
   g_object_unref(app);
 }
