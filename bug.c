6772ac9693eb1fc633623939752914385804b37e
JeromeA <jerome.abela@gmail.com>
2026-06-04 22:29:53 +0100

    Trim reproducer null end child

--- bug.c
+++ bug.c
@@ -13,7 +13,6 @@ static GtkWindow *create_window(void) {
 
   GtkWidget *middle_panel = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
-  gtk_paned_set_end_child(GTK_PANED(paned), NULL);
   gtk_box_append(GTK_BOX(drawer_host), middle_panel);
   gtk_paned_set_end_child(GTK_PANED(paned), drawer_host);
 
